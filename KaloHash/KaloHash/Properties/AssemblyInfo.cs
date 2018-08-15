f77066c4ec8b22eab81063bd937433248f080c71
Marc Kalo <marc.kalo@gmail.com>
2018-08-15 10:49:25 -0400

    moved back to .net framework 4.0, removed copyright statement from assembly properties.

--- KaloHash/KaloHash/Properties/AssemblyInfo.cs
+++ KaloHash/KaloHash/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Marc Kalo")]
 [assembly: AssemblyProduct("KaloHash")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("This is free and unencumbered software released into the public domain.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
